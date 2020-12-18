033593ab9782ac4466b293e95f6a0fb563458eb7
J3SUNG <wptjd6141@naver.com>
2020-12-18 04:58:57 -0800

    style change

--- index.js
+++ index.js
@@ -14,10 +14,31 @@ go.href = "https://j3sung.tistory.com/";
 go.target = "_blank";
 
 function handleClick() {
-  title.style.color = "green";
-  title.style.fontSize = "50px";
-  go.style.color = "red";
-  go.style.fontSize = "50px";
+  // const currentColor = title.style.color;
+  // if (currentColor === "green") {
+  //   title.style.color = "purple";
+  // } else {
+  //   title.style.color = "green";
+  // }
+  // title.style.fontSize = "50px";
+  // go.style.color = "red";
+  // go.style.fontSize = "50px";
+
+  // const currentClass = title.className;
+  // if (currentClass !== "clicked") {
+  //   title.className = "clicked";
+  // } else {
+  //   title.className = "";
+  // }
+
+  // const hasClass = title.classList.contains("clicked");
+  // if (!hasClass) {
+  //   title.classList.add("clicked");
+  // } else {
+  //   title.classList.remove("clicked");
+  // }
+
+  title.classList.toggle("clicked");
 }
 
 title.addEventListener("click", handleClick);
