1df77a08a630a064278c5ee7194954c323da7871
J3SUNG <wptjd6141@naver.com>
2020-12-18 04:19:47 -0800

    practice document

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-alert("Im Working. Im JS. Im Beautiful. Im worth it");
+console.log(window);
+console.log(window.document);
+console.log(document);
+console.log(document.getElementById("title"));
+
+const title = document.getElementById("title");
+title.innerHTML = "Hi";
+title.innerHTML += "  JavaScript!<br>";
+title.innerText += "Click here";
+
+const go = document.querySelector(".go");
+go.innerHTML = "Blog!";
+go.href = "https://j3sung.tistory.com/";
+go.target = "_blank";
+
+function handleClick() {
+  title.style.color = "green";
+  title.style.fontSize = "50px";
+  go.style.color = "red";
+  go.style.fontSize = "50px";
+}
+
+title.addEventListener("click", handleClick);
